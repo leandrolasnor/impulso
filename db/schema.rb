da0bc65d90e33754339efd4354124b16b52c0060
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-10-26 16:54:03 +0000

    chore: db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_201620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: :cascade do |t|
+    t.string "address", null: false
+    t.string "number"
+    t.string "district", null: false
+    t.string "city", null: false
+    t.string "state", null: false
+    t.string "zip", null: false
+    t.integer "proponent_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["zip"], name: "index_addresses_on_zip", unique: true
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "number", null: false
+    t.integer "proponent_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "proponents", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "taxpayer_number", null: false
+    t.date "birthdate", null: false
+    t.decimal "amount", precision: 10, scale: 2, null: false
+    t.decimal "discount_amount", precision: 10, scale: 2, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "addresses", "proponents"
+  add_foreign_key "contacts", "proponents"
 end
