614fa3669952112342b9e85b4e5d39b8edb62461
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-10-27 03:04:36 +0000

    refactor: destroy proponent serializer

--- app/services/http/destroy_proponent/serializer.rb
+++ app/services/http/destroy_proponent/serializer.rb
@@ -4,8 +4,18 @@ class Http::DestroyProponent::Serializer < ActiveModel::Serializer
   attributes :id,
              :name,
              :taxpayer_number,
-             :birthdate
+             :birthdate,
+             :amount,
+             :discount_amount
 
   has_many :addresses
   has_many :contacts
+end
+
+class AddressSerializer < ActiveModel::Serializer
+  attributes :id, :address, :number, :district, :city, :state, :zip
+end
+
+class ContactSerializer < ActiveModel::Serializer
+  attributes :id, :number
 end
