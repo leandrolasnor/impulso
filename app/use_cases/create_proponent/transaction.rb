69abc35c2314123bfc92f23176c21aa2dba72193
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-10-27 03:07:00 +0000

    fix: create proponent use case

--- app/use_cases/create_proponent/transaction.rb
+++ app/use_cases/create_proponent/transaction.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CreateProponent::Transaction
-  include Dry::Transaction(container: CreateProponentContainer)
+  include Dry::Transaction(container: CreateProponent::Container)
 
   step :validate, with: 'steps.validate'
   try :create, with: 'steps.create', catch: StandardError
