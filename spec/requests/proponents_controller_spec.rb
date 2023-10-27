827b66024572afec44a38131e55c79b09d7792c5
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2023-10-27 20:27:38 +0000

    test: integration test for proponents controller

--- spec/requests/proponents_controller_spec.rb
+++ spec/requests/proponents_controller_spec.rb
@@ -40,7 +40,6 @@ RSpec.describe ProponentsController do
 
   path '/v1/discount_amount/{amount}' do
     parameter name: 'amount', in: :path, type: :string, description: 'amount'
-
     get('discount_amount proponent') do
       tags 'Proponent'
       response(200, 'successful') do
@@ -150,10 +149,47 @@ RSpec.describe ProponentsController do
     end
   end
 
+  path '/v1/proponents/{id}/update_amount' do
+    patch('update proponent amount') do
+      tags 'Proponents'
+      consumes "application/json"
+      parameter name: :id, in: :path, type: :string, description: 'id'
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: { amount: { type: :number } },
+        required: [:amount]
+      }
+      response(200, 'successful') do
+        let(:proponent) { create(:proponent, amount: old_amount) }
+        let(:id) { proponent.id }
+        let(:old_amount) { 1320.40 }
+        let(:amount) { 2788.30 }
+        let(:params) { { amount: amount } }
+
+        let(:expected_body) do
+          {
+            id: be_a(Integer),
+            name: proponent.name,
+            taxpayer_number: proponent.taxpayer_number,
+            birthdate: proponent.birthdate.to_date.to_s,
+            amount: amount.to_s,
+            discount_amount: INSS[amount].to_s
+          }
+        end
+
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_body).to match(expected_body)
+          expect { proponent.reload }.to change(proponent, :amount).from(old_amount).to(amount)
+        end
+      end
+    end
+  end
+
   path '/v1/proponents/{id}' do
-    parameter name: 'id', in: :path, type: :string, description: 'id'
     get('show proponent') do
       tags 'Proponents'
+      parameter name: :id, in: :path, type: :string, description: 'id'
       response(404, 'not found') do
         let(:id) { 0 }
         let(:expected_body) { { error: I18n.t(:not_found) } }
@@ -188,38 +224,109 @@ RSpec.describe ProponentsController do
       end
     end
 
-    #   patch('update proponent') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
+    put('update proponent') do
+      tags 'Proponents'
+      consumes "application/json"
+      parameter name: :id, in: :path, type: :string, description: 'id'
+      parameter name: :params, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string, required: true },
+          birthdate: { type: :number, required: true },
+          contacts_attributes: {
+            type: :array,
+            items: {
+              type: :object,
+              properties: {
+                number: { type: :string }
+              }
+            },
+            required: [:number]
+          },
+          addresses_attributes: {
+            type: :array,
+            items: {
+              type: :object,
+              properties: {
+                address: { type: :string },
+                number: { type: :string },
+                district: { type: :string },
+                city: { type: :string },
+                state: { type: :string },
+                zip: { type: :string }
+              }
+            },
+            required: [:address, :number, :district, :city, :state, :zip]
+          }
+        },
+        required: [:name, :taxpayer_number, :birthdate, :amount]
+      }
+      response(404, 'not found') do
+        let(:id) { 0 }
+        let(:expected_body) { { error: I18n.t(:not_found) } }
+        let(:params) { nil }
 
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
-    #       run_test!
-    #     end
-    #   end
+        run_test! do |response|
+          expect(response).to have_http_status(:not_found)
+          expect(parsed_body).to eq(expected_body)
+        end
+      end
 
-    #   put('update proponent') do
-    #     response(200, 'successful') do
-    #       let(:id) { '123' }
+      response(200, 'successful') do
+        let(:proponent) { create(:proponent) }
+        let(:id) { proponent.id }
+        let(:name) { 'Proponent Name' }
+        let(:birthdate) { '1992-02-28' }
+        let(:contact) { build(:contact).attributes }
+        let(:address) { build(:address).attributes }
 
-    #       after do |example|
-    #         example.metadata[:response][:content] = {
-    #           'application/json' => {
-    #             example: JSON.parse(response.body, symbolize_names: true)
-    #           }
-    #         }
-    #       end
-    #       run_test!
-    #     end
-    #   end
+        let(:params) do
+          {
+            name: name,
+            birthdate: birthdate.to_date.to_s,
+            contacts_attributes: [contact],
+            addresses_attributes: [address]
+          }
+        end
+
+        let(:expected_body) do
+          {
+            id: be_a(Integer),
+            name: name,
+            taxpayer_number: proponent.taxpayer_number,
+            birthdate: birthdate,
+            amount: proponent.amount.to_s,
+            discount_amount: proponent.discount_amount.to_s,
+            contacts: [
+              {
+                id: be_a(Integer),
+                number: contact['number']
+              }
+            ],
+            addresses: [
+              {
+                id: be_a(Integer),
+                address: address['address'],
+                city: address['city'],
+                district: address['district'],
+                number: address['number'],
+                state: address['state'],
+                zip: address['zip']
+              }
+            ]
+          }
+        end
+
+        run_test! do |response|
+          expect(response).to have_http_status(:ok)
+          expect(parsed_body).to match(expected_body)
+        end
+      end
+    end
 
     delete('delete proponent') do
       tags 'Proponents'
+      parameter name: :id, in: :path, type: :string, description: 'id'
       response(200, 'successful') do
         let(:proponent) { create(:proponent) }
         let(:id) { proponent.id }
